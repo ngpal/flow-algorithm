f6b07367d14b7e26813e14de2bf87ac0cae64f29
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-04-02 22:13:54 +0530

    adt done

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ class Arc:
         self.capacity = capacity
         self.flow = 0
 
+    def is_recidual(self):
+        return self.capacity == 0
+
     def remaining(self):
         return self.capacity - self.flow
 
@@ -14,21 +17,30 @@ class Arc:
 class FlowGraph:
     def __init__(self):
         self.graph = {}
-
-    def add_node(self, id: int):
-        ...
-
-    def add_nodes(self, *ids):
-        map(self.add_node, ids)
+        self.sources = set()
+        self.sinks = set()
 
     def add_arc(self, a_id: int, b_id: int, capacity: int):
-        ...
+        if a_id not in self.graph:
+            self.graph[a_id] = {}
+            self.sources.add(a_id)
+        elif a_id in self.sinks:
+            self.sinks.remove(a_id)
 
-    # Checks whether a node is a source O(V + E)
+        if b_id not in self.graph:
+            self.graph[b_id] = {}
+            self.sinks.add(b_id)
+        elif b_id in self.sinks:
+            self.sources.remove(b_id)
+
+        self.graph[a_id][b_id] = Arc(capacity)
+        self.graph[b_id][a_id] = Arc(0)
+
+    # O(1) lookup
     def is_source(self, id: int) -> bool:
-        return True
+        return id in self.sources
 
-    # Checks whether a node is a sink O(1)
+    # O(1) lookup
     def is_sink(self, id: int) -> bool:
-        return True
+        return id in self.sinks
 
