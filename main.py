0772144d95080931d971210d5076ac3296b2b725
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-04-02 22:33:14 +0530

    setup

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # Arc is an edge
 class Arc:
-    def __init__(self, capacity):
+    def __init__(self, capacity, residual=None):
         self.capacity = capacity
+        self.residual = residual
         self.flow = 0
 
-    def is_recidual(self):
+    def is_residual(self):
         return self.capacity == 0
 
     def remaining(self):
         return self.capacity - self.flow
 
     def is_full(self) -> bool:
-        return self.remaining == 0
+        return self.remaining() == 0
+
+    def augment(self, flow: int):
+        self.flow += flow
+        self.residual.flow -= flow
 
 
 class FlowGraph:
@@ -30,11 +35,15 @@ class FlowGraph:
         if b_id not in self.graph:
             self.graph[b_id] = {}
             self.sinks.add(b_id)
-        elif b_id in self.sinks:
+        elif b_id in self.sources:
             self.sources.remove(b_id)
 
-        self.graph[a_id][b_id] = Arc(capacity)
         self.graph[b_id][a_id] = Arc(0)
+        self.graph[a_id][b_id] = Arc(capacity, self.graph[b_id][a_id])
+
+    def add_arcs(self, *arcs):
+        for arc in arcs:
+            self.add_arc(*arc)
 
     # O(1) lookup
     def is_source(self, id: int) -> bool:
@@ -44,3 +53,28 @@ class FlowGraph:
     def is_sink(self, id: int) -> bool:
         return id in self.sinks
 
+    def print(self):
+        print("sources:", self.sources)
+        print("sinks:", self.sinks)
+        for a, neighbours in self.graph.items():
+            for b, e in neighbours.items():
+                if not e.is_residual():
+                    print(f"{a} -> {b} | {e.flow}/{e.capacity}")
+
+
+if __name__ == "__main__":
+    g = FlowGraph()
+
+    g.add_arcs(
+        (0, 1, 3),
+        (0, 2, 7),
+        (2, 1, 5),
+        (1, 3, 3),
+        (1, 4, 4),
+        (2, 4, 3),
+        (3, 4, 3),
+        (3, 5, 2),
+        (4, 5, 6),
+    )
+
+    g.print()
